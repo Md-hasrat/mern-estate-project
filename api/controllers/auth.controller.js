ef484f9f33cc3d370ee8673d0c82d713e7c4f922
Md-hasrat <hasrat233222@gmail.com>
2023-12-08 23:05:39 +0530

    create a middleware and function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,6 +10,5 @@ export const signup = async (req, res, next) => {
       await newUser.save();
       res.status(201).json('User created successfully!');
     } catch (error) {
-      res.status(500).json(error.message)
-    }
+        next(error);    }
   };
