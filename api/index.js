d3a2f8a174ac77b1bb8a7db66071f768f952bc7c
Md-hasrat <hasrat233222@gmail.com>
2023-12-08 22:59:39 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -25,3 +26,4 @@ app.listen(3000, () => {
   });
 
   app.use('/api/user', userRouter);
+  app.use('/api/auth', authRouter);
