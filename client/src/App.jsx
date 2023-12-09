490e01766de0573421fed642e7b902e093253de1
Md-hasrat <hasrat233222@gmail.com>
2023-12-09 22:24:19 +0530

    update the heasder and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/signUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
  export default function App() {
    return (
      <BrowserRouter> 
@@ -15,7 +16,9 @@ import Header from './components/Header'
       <Route path='/sign-in' element={<Signin />}/>
       <Route path='/sign-up' element={<SignUp />}/>
       <Route path='/about' element={<About />} />
+      <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />} />
+      </Route>
      </Routes>
      </BrowserRouter>
    )
