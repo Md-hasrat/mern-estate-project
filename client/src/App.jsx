2b67172232812f2e18fe5c3c5bcf7f1f5a1f2018
Md-hasrat <hasrat233222@gmail.com>
2023-12-13 19:10:55 +0530

    complete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
  export default function App() {
    return (
      <BrowserRouter> 
@@ -18,6 +19,9 @@ import UpdateListing from './pages/UpdateListing'
       <Route path='/sign-in' element={<Signin />}/>
       <Route path='/sign-up' element={<SignUp />}/>
       <Route path='/about' element={<About />} />
+      <Route path='/listing/:listingId' element={<Listing />} />
+
+
       <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing />} />
