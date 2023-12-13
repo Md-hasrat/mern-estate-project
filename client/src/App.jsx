e81885c4ece4330c23520dd9e9eb6b82cd0ef106
Md-hasrat <hasrat233222@gmail.com>
2023-12-13 22:41:31 +0530

    create the show listing UI , APi and functionality and item component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
  export default function App() {
    return (
      <BrowserRouter> 
@@ -19,9 +20,8 @@ import Listing from './pages/Listing'
       <Route path='/sign-in' element={<Signin />}/>
       <Route path='/sign-up' element={<SignUp />}/>
       <Route path='/about' element={<About />} />
+      <Route path='/search' element={<Search />} />
       <Route path='/listing/:listingId' element={<Listing />} />
-
-
       <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing />} />
