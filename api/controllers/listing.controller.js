a2c3d378b9a37b933335f35d578802b2499fe517
Md-hasrat <hasrat233222@gmail.com>
2023-12-13 16:02:16 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -47,6 +48,18 @@ export const updateListing = async (req, res, next) => {
         { new: true }
       );
       res.status(200).json(updatedListing);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const getListing = async (req, res, next) => {
+    try {
+      const listing = await Listing.findById(req.params.id);
+      if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+      }
+      res.status(200).json(listing);
     } catch (error) {
       next(error);
     }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './pages/About'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
  export default function App() {
    return (
      <BrowserRouter> 
@@ -20,6 +21,10 @@ import CreateListing from './pages/CreateListing'
       <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing />} />
+      <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
 
       </Route>
      </Routes>
