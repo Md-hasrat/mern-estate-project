2c3b5656b09cc2766cdd4ca8c10b8a4c32f8d2b9
Md-hasrat <hasrat233222@gmail.com>
2023-12-13 23:20:28 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,17 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
-
-
-
-const app = express();
-
-app.use(express.json());
-
-app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
@@ -24,21 +17,35 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!');
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-
-  app.use('/api/user', userRouter);
-  app.use('/api/auth', authRouter);
-  app.use('/api/listing', listingRouter);
-
-
-  app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  });
+});
